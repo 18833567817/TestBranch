8f3c320dc2ed345ded8f0668d57e24c3dfc5de92
hanxudong <18833567817@163.com>
2017-08-07 09:09:22 +0800

    删除试试

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function show(id){
-	var chils = id.childNodes;
-	chils[3].style.display = "block";
-}
-function hiddent(id) {
-	var chils = id.childNodes;
-	chils[3].style.display = "none";n
-}
--- js/test8-2.js
+++
@@ -1,7 +0,0 @@
-function chage_bg (id) {
-	id.style.background = 'url(testimg/cart-icon-bg.png) no-repeat';
-	id.style.backgroundSize = '100% 100%';
-}
-function chage_old (id) {
-	id.style.background = '';
-}
