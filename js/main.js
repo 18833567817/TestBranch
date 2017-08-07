fb3cf6bc609ab984fc4014cfc105fd90dad2f03d
hanxudong <18833567817@163.com>
2017-08-07 09:11:07 +0800

    重新测试

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function show(id){
+	var chils = id.childNodes;
+	chils[3].style.display = "block";
+}
+function hiddent(id) {
+	var chils = id.childNodes;
+	chils[3].style.display = "none";n
+}
---
+++ js/test8-2.js
@@ -0,0 +1,7 @@
+function chage_bg (id) {
+	id.style.background = 'url(testimg/cart-icon-bg.png) no-repeat';
+	id.style.backgroundSize = '100% 100%';
+}
+function chage_old (id) {
+	id.style.background = '';
+}
